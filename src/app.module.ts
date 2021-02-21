60688ad3149a098acd32dd3012da905eead7fb04
ana1155 <anastasiia.solop@softermii.com>
2021-02-21 14:09:53 +0200

    test db

--- src/app.module.ts
+++ src/app.module.ts
@@ -46,11 +46,15 @@ import { UploadsModule } from 'src/uploads/uploads.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+          }),
       synchronize: process.env.NODE_ENV !== 'production',
       logging: process.env.NODE_ENV === 'dev',
       entities: [
